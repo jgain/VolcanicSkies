18f7405d901149c1949346cd0f84d9b5cbeb9b6d
Cilliers Pretorius <pietersieliepcp@gmail.com>
2024-12-13 12:10:42 +0100

    Licencing jargon added to most source files.

--- src/Tests/CoordinateTest.cpp
+++ src/Tests/CoordinateTest.cpp
@@ -1,3 +1,24 @@
+﻿/*
+ * Volcanic Skies
+ * Copyright (C) 2024 P. Cilliers Pretorius, University of Cape Town
+ *
+ * This file is part of the Volcanic Skies project.
+ *
+ * Volcanic Skies is free software: you can redistribute it and/or modify it under the terms 
+ * of the GNU General Public License (GPL) as published by the Free Software 
+ * Foundation, either version 2 of the License, or (at your discretion) any later version.
+ *
+ * Volcanic Skies is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * Volcanic Skies in the LICENSE file. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Additional information and disclaimers regarding liability and third-party 
+ * components can be found in the NOTICE file included with this project.
+ *
+ */
 // File to run tests for the Coordinate class.
 // Cilliers Pretorius
 // 14 May 2021
